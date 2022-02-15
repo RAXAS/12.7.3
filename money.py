68b1e77c6fe296726208ba0651fa061c243919e7
Dmitry Byk <dima-pinsk@mail.ru>
2022-02-15 20:32:31 +0300

    Update money.py

--- money.py
+++ money.py
@@ -1,14 +1,15 @@
 per_cent = {"tbk" : 5.6, "skb": 5.9, "vtb": 4.28, "sber": 4.0} 
 
-money = float(input("Введите сумму, которую хотите вложить: "))
+money = int(input("Введите сумму, которую хотите вложить: "))
 
 tbk = float(per_cent.get("tbk"))
 skb = float(per_cent.get("skb"))
 vtb = float(per_cent.get("vtb"))
 sber = float(per_cent.get("sber"))
 
-deposit = [tbk * money, skb * money, vtb * money, sber * money]
+deposit = [int(tbk) * money, int(skb) * money, int(vtb) * money, int(sber) * money]
 
 a = int(max(deposit))
 
+print(deposit)
 print("Максимальная сумма, которую вы можете заработать — ", a)
